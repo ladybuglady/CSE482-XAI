1f49ef0ea697c20fd811f668075eaae21918c9c9
ladybuglady <ztoprakb@gmail.com>
2023-04-27 00:23:50 -0700

    update decomposer with 1 ecg

--- Attia/decomposer.py
+++ Attia/decomposer.py
@@ -18,7 +18,7 @@ from keras.utils import to_categorical
 from pandas import *
 
 # ~~~~~~~~~~~~~~~ DATA FETCH ~~~~~~~~~~~~~~~
-dir_path = '../../../../../../local1/CSE_XAI/study60_recordings_json/'
+dir_path = '../../../../../../local1/CSE_XAI/small_data/'
 
 data = read_csv("/content/drive/MyDrive/Colab Notebooks/XAI_DATA/Copy of study60_patient_recordings.csv")
  
@@ -27,6 +27,24 @@ filename_csv = data['recording_public_id'].tolist()
 y_csv = data['determination'].tolist()
 
 # get 1 file
+count = 0
+# Iterate directory
+for path in os.listdir(dir_path):
+    patient_X = np.empty((2, 5000))
+
+    jsonFile = open(dir_path + path, 'r')
+    fileContents = json.load(jsonFile)
+
+    # digging into the dictionaries to get lead data
+    lead_1_samples = fileContents['samples']
+    lead_2_samples = fileContents['extraLeads'][0]['samples']
+
+    # Crop the data to 5000 data points.
+    patient_X[0,:] = lead_1_samples[0:5000]
+    patient_X[1,:] = lead_2_samples[0:5000]
+    count += 1
+    if count == 1: # just get 1.
+        break
 
 '''
 After getting a file, put it into a numpy array
