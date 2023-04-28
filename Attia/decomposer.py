a5f4dcacb57d5ff9b026415e828ccf7244cccfd8
Nawaf Osman <nawaf3@nigiri.cs.washington.edu>
2023-04-27 23:52:00 -0700

    Implemented ventricular rate extraction and R-waves detection from input ECG leads in decomposer.py

--- Attia/decomposer.py
+++ Attia/decomposer.py
@@ -1,36 +1,21 @@
-
 # ~~~~~~~~~~~~~~~ IMPORTS ~~~~~~~~~~~~~~~
 from collections import Counter
 import numpy as np
-import keras
-from keras.callbacks import ModelCheckpoint,EarlyStopping,ReduceLROnPlateau
-from keras.models import Sequential,Model
-from keras.layers import concatenate,Activation,Dropout,Dense,ZeroPadding2D
-from keras.layers import Input,add,Conv2D, MaxPooling2D,Flatten,BatchNormalization,LSTM
 import os
 import zipfile
-from pandas import *
-from sklearn.model_selection import train_test_split
-from attia_6lead_model import BuildModel
 import json
-import os
-from keras.utils import to_categorical
 from pandas import *
+import matplotlib.pyplot as plt
+from biosppy.signals import ecg
 
 # ~~~~~~~~~~~~~~~ DATA FETCH ~~~~~~~~~~~~~~~
 dir_path = '../../../../../../local1/CSE_XAI/small_data/'
-
-data = read_csv("/content/drive/MyDrive/Colab Notebooks/XAI_DATA/Copy of study60_patient_recordings.csv")
- 
-# converting column data to list
-filename_csv = data['recording_public_id'].tolist()
-y_csv = data['determination'].tolist()
 
 # get 1 file
 count = 0
 # Iterate directory
 for path in os.listdir(dir_path):
-    patient_X = np.empty((2, 5000))
+    patient_X = np.empty((2, 6000))
 
     jsonFile = open(dir_path + path, 'r')
     fileContents = json.load(jsonFile)
@@ -39,11 +24,11 @@ for path in os.listdir(dir_path):
     lead_1_samples = fileContents['samples']
     lead_2_samples = fileContents['extraLeads'][0]['samples']
 
-    # Crop the data to 5000 data points.
-    patient_X[0,:] = lead_1_samples[0:5000]
-    patient_X[1,:] = lead_2_samples[0:5000]
+    # Crop the data to 6000 data points (6 seconds).
+    patient_X[0,:] = lead_1_samples[0:6000]
+    patient_X[1,:] = lead_2_samples[0:6000]
     count += 1
-    if count == 1: # just get 1.
+    if count == 2: # just get 1.
         break
 
 '''
@@ -52,3 +37,22 @@ Be able use the bio spy python library to get wave components from that numpy ar
 Output a list of wave component values with timestamp
 
 '''
+features = {"VR": None, "R": []}
+frequency = fileContents["frequency"]
+
+# smooth, straighten, and filter noise from leads
+filtered_lead_1 = ecg.ecg(patient_X[0], sampling_rate=frequency, show=False)[1]
+filtered_lead_2 = ecg.ecg(patient_X[1], sampling_rate=frequency, show=False)[1]
+
+# extract r-wave peak timestamps from filtered lead I
+r_peaks_lead_1 = ecg.engzee_segmenter(filtered_lead_1, sampling_rate=frequency)[0]
+
+# correct r-wave peak timestamps to fit original lead I
+r_peaks_lead_1 = ecg.correct_rpeaks(patient_X[0], r_peaks_lead_1, sampling_rate=frequency)[0]
+
+features["VR"] = len(r_peaks_lead_1) * (60 / (len(patient_X[0]) / 1000))
+features["R"] = r_peaks_lead_1
+
+plt.plot(patient_X[0])
+plt.xlim(0, 6000)
+plt.show()
