fc414a8f348711b3a5dd22868ae410f053cca2a9
Nawaf Osman <nawaf3@nigiri.cs.washington.edu>
2023-05-15 15:15:05 -0700

    Implemented timestamp  extraction of all ECG features in lead II (P, PR, QRS, ST, T, TP)

--- Attia/decomposer.py
+++ Attia/decomposer.py
@@ -42,28 +42,34 @@ Output a list of wave component values with timestamp
 
 def decompose(patient_X):
 
-    features = {"VR": None, "R": [], "P":[], "T":[]}
+    features = {"R1": [], "R2":[], "P":[], "PR":[], "QRS":[], "ST":[], "T":[], "TP":[]}
     frequency = fileContents["frequency"]
-    duration = len(patient_X[0])
 
-    # smooth and straighten lead I for R-peak detection
+    # smooth and straighten leads for inversion detection
     filtered_lead_1 = ecg.ecg(patient_X[0], sampling_rate=frequency, show=False)[1]
+    filtered_lead_2 = ecg.ecg(patient_X[1], sampling_rate=frequency, show=False)[1]
 
-    # check if lead 1 is inverted
-    inverted = nk.ecg_invert(patient_X[0], sampling_rate=frequency, show=False)[1]
+    # check if leads are inverted
+    inverted_lead_1 = nk.ecg_invert(filtered_lead_1, sampling_rate=frequency, show=False)[1]
+    inverted_lead_2 = nk.ecg_invert(filtered_lead_2, sampling_rate=frequency, show=False)[1]
 
-    # extract r-wave peak timestamps from filtered lead I using Hamilton's algorithm
-    r_peaks_lead_1 = ecg.hamilton_segmenter(filtered_lead_1, sampling_rate=frequency)[0]
-
-    if inverted:
+    if inverted_lead_1:
+        # extract r-wave peak timestamps from inverted lead I (the inversion of the originally inverted lead)
+        r_peaks_lead_1 = nk.ecg_peaks(np.negative(patient_X[0]), sampling_rate=frequency, method="neurokit", correct_artifacts=True)[1]["ECG_R_Peaks"]
         # correct r-wave peak timestamps to fit inverted lead I (the inversion of the originally inverted lead)
         r_peaks_lead_1 = ecg.correct_rpeaks(np.negative(patient_X[0]), r_peaks_lead_1, sampling_rate=frequency)[0]
     else:
-        # correct r-wave peak timestamps to fit original lead I
+        # extract r-wave peak timestamps from lead I
+        r_peaks_lead_1 = nk.ecg_peaks(patient_X[0], sampling_rate=frequency, method="neurokit", correct_artifacts=True)[1]["ECG_R_Peaks"]
+        # correct r-wave peak timestamps
         r_peaks_lead_1 = ecg.correct_rpeaks(patient_X[0], r_peaks_lead_1, sampling_rate=frequency)[0]
 
-    features["VR"] = len(r_peaks_lead_1) * (60 / (len(patient_X[0]) / 1000))
-    features["R"] = r_peaks_lead_1
+    if inverted_lead_2:
+        # correct r-wave peak timestamps to fit inverted lead II (the inversion of the originally inverted lead)
+        r_peaks_lead_2 = ecg.correct_rpeaks(np.negative(patient_X[1]), r_peaks_lead_1, sampling_rate=frequency)[0]
+    else:
+        # correct r-wave peak timestamps to fit lead II
+        r_peaks_lead_2 = ecg.correct_rpeaks(patient_X[1], r_peaks_lead_1, sampling_rate=frequency)[0]
 
     # filter out baseline wander in lead II
     filtered_lead_2 = nk.signal_filter(patient_X[1], sampling_rate=frequency, lowcut=3, highcut=None, method="butterworth", order=2, show=False)
@@ -75,32 +81,79 @@ def decompose(patient_X):
 
     # filter out electrode motion artifacts in lead II
 
-    # extract p-wave peak timestamps from filtered lead II
-    peaks_lead_2 = nk.ecg_delineate(filtered_lead_2, r_peaks_lead_1, sampling_rate=frequency, method="cwt", show=False, check=False)[0]
+    # extract p_positions-wave peak timestamps from filtered lead II
+    if inverted_lead_2:
+        peaks_lead_2 = nk.ecg_delineate(np.negative(filtered_lead_2), r_peaks_lead_2, sampling_rate=frequency, method="dwt", show=False, check=False)[0]
+    else:
+        peaks_lead_2 = nk.ecg_delineate(filtered_lead_2, r_peaks_lead_2, sampling_rate=frequency, method="dwt", show=False, check=False)[0]
 
-    p_peaks_lead_2 = peaks_lead_2["ECG_P_Peaks"]
-    t_peaks_lead_2 = peaks_lead_2["ECG_T_Peaks"]
+    p_starts_lead_2 = []
+    p_ends_lead_2 = []
 
-    # estimate start and end timestamps based on p-wave peak and add to features map
-    for i, p in enumerate(p_peaks_lead_2):
-        if p == 0:
-            continue
-        if i - 60 < 0:
-            features["P"].append([0, i])
-        elif i + 60 > duration:
-            features["P"].append([i, duration])
-        else:
-            features["P"].append([i - 30, i + 30])
+    for i, v in enumerate(peaks_lead_2["ECG_P_Onsets"]):
+        if v == 1:
+            p_starts_lead_2.append(i)
 
-    # estimate start and end timestamps based on t-wave peak and add to features map
-    for i, t in enumerate(t_peaks_lead_2):
-        if t == 0:
-            continue
-        if i - 60 < 0:
-            features["T"].append([0, i])
-        elif i + 60 > duration:
-            features["T"].append([i, duration])
-        else:
-            features["T"].append([i - 30, i + 30])
+    for i, v in enumerate(peaks_lead_2["ECG_P_Offsets"]):
+        if v == 1:
+            p_ends_lead_2.append(i)
+
+    p_positions = list(zip(p_starts_lead_2, p_ends_lead_2))
+
+    q_starts_lead_2 = []
+    s_ends_lead_2 = []
+
+    for i, v in enumerate(peaks_lead_2["ECG_R_Onsets"]):
+        if v == 1:
+            q_starts_lead_2.append(i)
+
+    for i, v in enumerate(peaks_lead_2["ECG_R_Offsets"]):
+        if v == 1:
+            s_ends_lead_2.append(i)
+
+    if q_starts_lead_2[0] < p_ends_lead_2[0]:
+        pr_positions = list(zip(p_ends_lead_2, q_starts_lead_2[1:]))
+    else:
+        pr_positions = list(zip(p_ends_lead_2, q_starts_lead_2))
+
+    if q_starts_lead_2[0] > s_ends_lead_2[0]:
+        qrs_positions = list(zip(q_starts_lead_2, s_ends_lead_2[1:]))
+    else:
+        qrs_positions = list(zip(q_starts_lead_2, s_ends_lead_2))
+
+    t_starts_lead_2 = []
+    t_ends_lead_2 = []
+
+    for i, v in enumerate(peaks_lead_2["ECG_T_Onsets"]):
+        if v == 1:
+            t_starts_lead_2.append(i)
+
+    for i, v in enumerate(peaks_lead_2["ECG_T_Offsets"]):
+        if v == 1:
+            t_ends_lead_2.append(i)
+
+    if t_starts_lead_2[0] < s_ends_lead_2[0]:
+        st_positions = list(zip(s_ends_lead_2, t_starts_lead_2[1:]))
+    else:
+        st_positions = list(zip(s_ends_lead_2, t_starts_lead_2))
+
+    t_positions = list(zip(t_starts_lead_2, t_ends_lead_2))
+
+    for i in t_positions:
+        plt.axvspan(i[0], i[1], facecolor="r", alpha=0.25)
+
+    if t_starts_lead_2[0] > p_ends_lead_2[0]:
+        tp_positions = list(zip(t_ends_lead_2, p_starts_lead_2[1:]))
+    else:
+        tp_positions = list(zip(t_ends_lead_2, p_starts_lead_2))
+    
+    features["R1"] = r_peaks_lead_1
+    features["R2"] = r_peaks_lead_2
+    features["P"] = p_positions
+    features["PR"] = pr_positions
+    features["QRS"] = qrs_positions
+    features["ST"] = st_positions
+    features["T"] = t_positions
+    features["TP"] = tp_positions
 
     return features
